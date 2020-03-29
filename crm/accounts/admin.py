d1cf95f3b47fe8269b14a7563f9a648688de1637
Henkho Dryza <simangahk@gmail.com>
2020-03-29 04:16:13 -0700

    intro to the database-

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import inspect
+from . import models
 
+for name, obj in inspect.getmembers(models):
+    if inspect.isclass(obj):
+        admin.site.register(obj)
 # Register your models here.
